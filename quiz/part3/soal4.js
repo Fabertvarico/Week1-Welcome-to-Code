44cb15be736cc019c399678187f8a81dc36c277f
Fabertvarico <fabertvaricodigital@gmail.com>
2025-03-29 19:07:52 +0700

    soal4 part 3

---
+++ quiz/part3/soal4.js
@@ -0,0 +1,15 @@
+for (let i = 1; i <= 100; i += 2) {
+    if (i %3 == 0) {
+        console.log("kelipatan 3 dengan pertambahan 2 : ", i)
+    }
+}
+for (let i = 1; i <= 100; i += 5) {
+    if (i %6 == 0) {
+        console.log("kelipatan 6 dengan pertambahan 5 : ", i)
+    }
+}
+for (let i = 1; i <= 100; i += 9) {
+    if (i %10 == 0) {
+        console.log("kelipatan 10 dengan pertambahan 9 : ", i)
+    }
+}
