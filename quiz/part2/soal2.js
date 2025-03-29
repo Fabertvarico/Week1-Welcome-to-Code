c88a57ef0466e53015ea8b523d9d1c216acd2c6d
Fabertvarico <fabertvaricodigital@gmail.com>
2025-03-29 18:27:09 +0700

    soal2 part 2

---
+++ quiz/part2/soal2.js
@@ -0,0 +1,3 @@
+let word = 'wow JavaScript is so cool';
+let exampleFirstWord = word[0] + word[1] + word[2] + " " +  word[4] + word[5] + word[6] + word[7] + word[8] + word[9] + word[10] + word[11] + word[12] + word[13] + " " + word[15] + word[16] + " " + word[18] + word [19] + " " + word[21] + word[22] + word[23] + word[24];
+console.log(exampleFirstWord)
