5bc788bc711a395049eb845bc337b89fdf4cca82
Fabertvarico <fabertvaricodigital@gmail.com>
2025-03-29 18:34:14 +0700

    soal4 part 2

---
+++ quiz/part2/soal4.js
@@ -0,0 +1,6 @@
+let word4 = 'wow JavaScript is so cool';
+let exampleFirstWord4 = word4.substring(0, 3) + " " +  word4.substring(4, 14) + " " + word4.substring(15, 17) + " " + word4.substring(18, 20) + " " + word4.substring(21, 25);
+
+let firstWordLength = exampleFirstWord4.length;
+
+console.log('First Word: ' + exampleFirstWord4 + ', with length: ' + firstWordLength);
